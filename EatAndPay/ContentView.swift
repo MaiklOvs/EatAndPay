2916e619c2df8bd16d083ba3c3510d5c30325562
ovsyannikov.m10 <ovsyannikov.m10@wb.ru>
2026-06-26 12:57:24 +0300

    Initial Commit

---
+++ EatAndPay/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  EatAndPay
+//
+//  Created by Ovsyannikov.M10 on 26.06.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ EatAndPay/EatAndPayApp.swift
@@ -0,0 +1,17 @@
+//
+//  EatAndPayApp.swift
+//  EatAndPay
+//
+//  Created by Ovsyannikov.M10 on 26.06.2026.
+//
+
+import SwiftUI
+
+@main
+struct EatAndPayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
